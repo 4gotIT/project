2cb29ee7b4d8f68cd4ce183fecc1de88ffd653be
Aziz <ibodov.aziz@yandex.ru>
2023-10-16 14:16:45 +0300

    add substraction func

--- func.py
+++ func.py
@@ -1,6 +1,12 @@
-def func(a, b):
+def func_addition(a, b):
     if (isinstance(a, int) or isinstance(a, float)) and (isinstance(b, int) or isinstance(b, float)):
         return a + b
     else:
         return 'Ошибка'
 
+
+def func_subtraction(a, b):
+    if (isinstance(a, int) or isinstance(a, float)) and (isinstance(b, int) or isinstance(b, float)):
+        return a + b
+    else:
+        return 'Ошибка'
